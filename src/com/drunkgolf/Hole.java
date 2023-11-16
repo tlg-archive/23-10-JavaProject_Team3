5e55a2f2f0d26533e82b6969eab64b8346454c87
Sam Kibbett <Sam.Kibbett@tlgcohort.com>
2023-11-16 14:13:02 -0700

    Hole class framework

--- src/com/drunkgolf/Hole.java
+++ src/com/drunkgolf/Hole.java
@@ -1,5 +1,40 @@
 package com.drunkgolf;
 
 class Hole {
+    private int par;
+    private int swingsTaken;
+    private int distanceToHole;
 
+    // ctor
+    public Hole(int par, int initialDistance) {
+        this.par = par;
+        this.swingsTaken = 0;
+        this.distanceToHole = initialDistance;
+    }
+
+    // score
+    public int score() {
+        return swingsTaken - par;
+    }
+
+    // method to update the distance after swing
+    private void updateDistance(int distance) {
+        distanceToHole -= distance;
+    }
+
+    public boolean endHole() {
+        return swingsTaken >= par + 2;
+    }
+
+    public int getPar() {
+        return par;
+    }
+
+    public int getSwingsTaken() {
+        return swingsTaken;
+    }
+
+    public int getDistanceToHole() {
+        return distanceToHole;
+    }
 }
