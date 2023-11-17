36caf97be32a0473f45796982a630c435825c49f
Aaron Day <aaron.day@tlgcohort.com>
2023-11-17 15:12:51 -0800

    Balance Update 1.1

--- src/com/drunkgolf/ClubType.java
+++ src/com/drunkgolf/ClubType.java
@@ -4,13 +4,14 @@ package com.drunkgolf;
 public enum ClubType {
     DRIVER(200, 300),
     IRON(100, 180),
-    WEDGE(20, 80),
-    PUTTER(1, 15);
+    WEDGE(30, 90),
+    PUTTER(6, 20);
 
     public int minRange;
     public int maxRange;
 
     ClubType(int minRange, int maxRange) {
+
         this.minRange = minRange;
         this.maxRange = maxRange;
     }
